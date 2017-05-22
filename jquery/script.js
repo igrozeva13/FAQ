156ba42a0f4177845d9c648592f0c649fc9ee391
Staff <staff@Radostinas-MacBook-Pro.local>
2017-05-22 16:48:14 -0400

    added tablet size queries

--- jquery/script.js
+++ jquery/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-// --------NAV ---------------------//
+// --------NAV ---------------------
   $(".dropbtn").click(function(){
       $("#myDropdown").slideToggle();
   });
@@ -13,13 +13,31 @@ $(document).ready(function(){
         $("#plus-1").slideUp(0);
     });
 
+    $("#question-1").click(function(){
+        $("#answer-box-1").slideDown(750);
+        $("#minus-1").slideDown(0);
+        $("#plus-1").slideUp(0);
+    });
+
     $("#minus-1").click(function(){
         $("#answer-box-1").slideUp(750);
         $("#minus-1").slideUp(0);
         $("#plus-1").slideDown(0);
     });
 
+    $("#answer-1").click(function(){
+        $("#answer-box-1").slideUp(750);
+        $("#minus-1").slideUp(0);
+        $("#plus-1").slideDown(0);
+    });
+
     $("#plus-2").click(function(){
+        $("#answer-box-2").slideDown(750);
+        $("#minus-2").slideDown(0);
+        $("#plus-2").slideUp(0);
+    });
+
+    $("#question-2").click(function(){
         $("#answer-box-2").slideDown(750);
         $("#minus-2").slideDown(0);
         $("#plus-2").slideUp(0);
@@ -37,6 +55,12 @@ $(document).ready(function(){
         $("#plus-3").slideUp(0);
     });
 
+    $("#question-3").click(function(){
+        $("#answer-box-3").slideDown(750);
+        $("#minus-3").slideDown(0);
+        $("#plus-3").slideUp(0);
+    });
+
     $("#minus-3").click(function(){
         $("#answer-box-3").slideUp(750);
         $("#minus-3").slideUp(0);
@@ -49,6 +73,13 @@ $(document).ready(function(){
         $("#plus-4").slideUp(0);
     });
 
+    $("#question-4").click(function(){
+        $("#answer-box-4").slideDown(750);
+        $("#minus-4").slideDown(0);
+        $("#plus-4").slideUp(0);
+    });
+
+
     $("#minus-4").click(function(){
         $("#answer-box-4").slideUp(750);
         $("#minus-4").slideUp(0);
@@ -56,6 +87,12 @@ $(document).ready(function(){
     });
 
     $("#plus-5").click(function(){
+        $("#answer-box-5").slideDown(750);
+        $("#minus-5").slideDown(0);
+        $("#plus-5").slideUp(0);
+    });
+
+    $("#question-5").click(function(){
         $("#answer-box-5").slideDown(750);
         $("#minus-5").slideDown(0);
         $("#plus-5").slideUp(0);
@@ -73,6 +110,12 @@ $(document).ready(function(){
         $("#plus-6").slideUp(0);
     });
 
+    $("#question-6").click(function(){
+        $("#answer-box-6").slideDown(750);
+        $("#minus-6").slideDown(0);
+        $("#plus-6").slideUp(0);
+    });
+
     $("#minus-6").click(function(){
         $("#answer-box-6").slideUp(750);
         $("#minus-6").slideUp(0);
@@ -80,6 +123,12 @@ $(document).ready(function(){
     });
 
     $("#plus-7").click(function(){
+        $("#answer-box-7").slideDown(750);
+        $("#minus-7").slideDown(0);
+        $("#plus-7").slideUp(0);
+    });
+
+    $("#question-7").click(function(){
         $("#answer-box-7").slideDown(750);
         $("#minus-7").slideDown(0);
         $("#plus-7").slideUp(0);
